a4978a4ea7bcaea238a6f50d922e0a18f9715392
parazitol-official <73265222+parazitol-official@users.noreply.github.com>
2020-10-23 03:54:14 +0300

    1

---
+++ js/translater.js
@@ -0,0 +1,253 @@
+var new_lang = {
+
+"lt1": "SILNÝ ÚDER PARAZITŮM",
+
+"lt2": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Normalizuje funkci trávícího traktu",
+
+"lt3": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Ničí všechny druhy cizopasníků",
+
+"lt4": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Odstraňuje produkty životní činnosti parazitů",
+
+"lt5": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Předchází opakované nákaze",
+
+"lt6": "SPECIÁLNÍ<br>NABÍDKA",
+
+"lt7": "do konce akce zbývá",
+
+"lt8": "<div class=\"price_old\"></div>  Běžná cena",
+
+"lt9": "<div class=\"price_main wow pulse\" data-wow-delay=\"2s\" data-wow-iteration=\"3\" style=\"visibility: visible; animation-delay: 2s; animation-iteration-count: 3; animation-name: pulse;\"></div>  Cena v akci",
+
+"lt10": "Objednat",
+
+"lt11": "PODLE POSLEDNÍCH ÚDAJŮ WHO<br>JE VÍCE NEŽ MILIARDA LIDÍ NAKAŽENA PARAZITY",
+
+"lt12": "90% z nich ani nenapadlo, že se stali prostředím výživy pro cizopasníky",
+
+"lt13": "JSTE SI JISTI, ŽE K JEJICH MNOŽSTVÍ NEPATŘÍTE? ZKONTROLUJTE SE NA PŘÍZNAKY NÁKAZY!",
+
+"lt14": "PŘÍZNAKY HELMINTÓZY",
+
+"lt15": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Pocit únavy",
+
+"lt16": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Bolesti hlavy, zdravotní potíže",
+
+"lt17": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Zánět horních cest dýchacích",
+
+"lt18": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Podrážděnost, agresivita",
+
+"lt19": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Všechny možné vyrážky na kůži",
+
+"lt20": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Snížení tělesné hmotnosti, dokonce i při dobré chuti k jídlu",
+
+"lt21": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Problémy se spánkem",
+
+"lt22": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Alergie (slzící oči, vyrážky, rýma)",
+
+"lt23": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Častá nachlazení, angína, ucpaný nos",
+
+"lt24": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Bolesti v kloubech a svalech",
+
+"lt25": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Zduření lymfatických uzlin",
+
+"lt26": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Poruchy trávení: nadýmání, zácpa nebo průjem",
+
+"lt27": "ZDROJE NAKAŽENÍ CIZOPASNÍKY",
+
+"lt28": "Špatně umytá zelenina, ovoce, jahody",
+
+"lt29": "Špatně propečené maso a ryby",
+
+"lt30": "Nakažená voda nebo půda",
+
+"lt31": "Kontakty s domácími zvířaty",
+
+"lt32": "Veřejné prostory a doprava",
+
+"lt33": "Kousnutí hmyzem",
+
+"lt34": "Restaurace a provozovny rychlého občerstvení",
+
+"lt35": "ZAČNĚTE LÉČBU JIŽ TEĎ!",
+
+"lt36": "ZAČÍT",
+
+"lt37": "Paraziti, <br>kteří způsobují strašné komplikace",
+
+"lt38": "CYSTICERKY",
+
+"lt39": "Nejčastěji (více než 60%) se cysticerky dostávají do mozku, méně často do kosterních svalů a očí. Žije 5 až 30 let, způsobuje<span> rakovinové nádory</span> ",
+
+"lt40": "ROUPI",
+
+"lt41": "Obývají a množí se v orgánech trávícího traktu. Způsobují vznik<span>vředů</span> a<span>rozsáhlé krvácení</span> ",
+
+"lt42": "ANCYLOSTOMA",
+
+"lt43": "Poškozují sliznici a živí se krví, která se při tom uvolňuje Zanechávají krvácející rány, což vede k rozvoji<span>těžké anémii</span> a <span>sepse.</span> ",
+
+"lt44": "ALVEOKOK",
+
+"lt45": "Dochází k odumírání tkáně nakaženého orgánu. Kromě toho dochází k poškození a<span> ukončení činnosti krevních cév</span> ",
+
+"lt46": "MULTIAKTIVNÍ ÚČINEK <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">XX</span> ",
+
+"lt47": "NIČÍ",
+
+"lt48": "všechny možné druhy invaze v nejkratší době",
+
+"lt49": "ODSTRAŇUJE",
+
+"lt50": "produkty životní činnosti parazitů z organizmu",
+
+"lt51": "NAPOMÁHÁ",
+
+"lt52": "vyčištění organizmu od toxinů",
+
+"lt53": "OBNOVUJE",
+
+"lt54": "fungování imunitního, endokrinologického, trávícího, nervového, kardiovaskulárního systému",
+
+"lt55": "CHRÁNÍ",
+
+"lt56": "před opakovanou nákazou, formuje patogenní prostředí v lidském organizmu a posiluje imunitu",
+
+"lt57": "OBJEDNAT",
+
+"lt58": "SILNÉ PŘÍRODNÍ SLOŽENÍ <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">XX</span> ",
+
+"lt59": "MORINGA",
+
+"lt60": "Díky svému unikátnímu složení čistí organizmus od škodlivých látek a těžkých kovů. Efektivně bojuje s parazity v trávícím traktu, ničí cizí DNA. Čistí střeva a žlučové cesty.",
+
+"lt61": "PAU D´ARCO",
+
+"lt62": "Lapachol - speciální sloučenina, obsažená v kůře brání rozmnožování patogenních mikroorganizmů, inhibuje rozmnožování a rozvoj intracelulárních parazitů.",
+
+"lt63": "ČERNÝ OŘECH",
+
+"lt64": "Prvky černého ořechu narušují látkovou výměnu červů, způsobují ochrnutí svalových tkání parazitů, dále zvyšují imunitu a obranné funkce organizmu, čistí krev od toxinů.",
+
+"lt65": "LISTY OLIVY",
+
+"lt66": "ObsahujÍ oleuropein, který má antiparazitické a protiplísňové účinky, snižuje oxidační poškození v krvi, játrech, ledvinách a mozkových tkáních.",
+
+"lt67": "KOŘEN KURKUMY",
+
+"lt68": "V oddenku kurkumy je obsažen cineol, kurkumín a vitamín P. Cineol normalizuje fungování trávících orgánů a ztěžuje životní činnost červů, kurkumín má antiparazitické účinky vůči hlístům, vitamín P obnovuje fungování orgánů krvetvorby a čistí krev.",
+
+"lt69": "LOČIDLO",
+
+"lt70": "Je známé díky svým velmi silným baktericidním a antimikrobiálním vlastnostem Zvláště se oceňuje jeho schopnost vylučovat tasemnice - parazity z trávícího traktu.",
+
+"lt71": "ČÍM <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">XX</span> JE LEPŠÍ NEŽ JINÉ PREPARÁTY?",
+
+"lt72": "CHEMICKÉ PREPARÁTY",
+
+"lt74": "CHEMICKÉ PREPARÁTY",
+
+"lt75": "Nebezpečné chemické <br>látky ve složení <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt76": "Dopad na vnitřní <br>orgány a systémy <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt77": "Napomáhají<br>snížení imunity <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt78": "Poškozují orgány trávícího traktu:<br> <span>játra, žaludek, slinivku břišní, střeva, vyvolávají<br> gastritidu a vředy.</span> ",
+
+"lt80": "Plně<br>přírodní složení <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt81": "Nemá vedlejší<br>účinky, chrání<br>vnitřní orgány <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt82": "Zvyšuje imunitu<br>a odolnost <br>organizmu vůči infekcím <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt83": "Má blahodárný účinek na trávící trakt:<br> <span>, zlepšuje odtok žluči, stimuluje fungování střev</span> ",
+
+"lt84": " <span>XX</span> MĚKCE PRO ČLOVĚKA, TĚŽKO PRO CIZOPASNÍKY",
+
+"lt85": "„Většina lidí ani nemá podezření, že organizmus každého druhého je NAKAŽEN PARAZITY.",
+
+"lt86": "V 95% případů přítomnost cizopasníků nezpůsobuje žádné příznaky, dokud nezpůsobí nenapravitelnou škodu vnitřním orgánům a nenaruší fungování všech systémů organizmu.",
+
+"lt87": "K dnešnímu dni je pouze preparát XX schopen ÚPLNĚ vyčistit organizmus od jakýchkoli parazitů, kterých existuje více než 250 druhů. Vytváří v lidském organizmu prostředí, které narušuje samotný proces parazitování.",
+
+"lt88": "Zkušenosti mých pacientů potvrzují, že XX je nejúčinější a bezpečný prostředek pro zničení a odstranění VŠECH invazních druhů, včetně jejich vajíček, což nedovoluje opakované nakažení.",
+
+"lt89": "Pozitivního efektu je dosaženo ve 100% případů“.",
+
+"lt90": "Michal Hrnčíř, lékař parazitolog",
+
+"lt91": "DALŠÍ ÚČINKY <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">XX</span> ",
+
+"lt92": "Napomáhá čištění orgánů a tkání, neutralizuje intoxikaci, která je následkem parazitární infekce",
+
+"lt93": "Regeneruje sliznice, čistí krev, lymfu, mezibuněčné tekutiny",
+
+"lt94": "Normalizuje oxidačně obnovitelnou rovnováhu, hrozící vznikem rakovinotvorných buněk ",
+
+"lt95": "Normalizuje oxidačně obnovitelnou rovnováhu, hrozící vznikem rakovinotvorných buněk ",
+
+"lt96": "Napomáhá odstranění vředů a gastritidy, zabíjí bakterie, které je způsobují",
+
+"lt97": "Obnovuje a normalizuje funkce poškozených orgánů",
+
+"lt98": "Má blahodárný vliv na orgány trávícího traktu",
+
+"lt99": "Zlepšuje stav kůže, vlasů, nehtů",
+
+"lt100": "OBJEDNAT",
+
+"lt101": "OHLASY KLIENTŮ",
+
+"lt102": "<div class=\"slide-description-title\">Helena, Brno</div>  „Tento prostředek se mi opravdu zalíbil. Pili jsme ho s manželem oba. U každého z nás se to projevovalo jinak: objevoval se studený pot, byla řídká stolice, objevilo se i trochu akné a hrozná bolest hlavy. Už po 1,5 týdne zhoršení pominulo a teď se oba cítíme skvěle! XX funguje velmi delikátně, ale silně a sebevědomě“.",
+
+"lt103": "<div class=\"slide-description-title\">Petra, Ústí nad Orlicí</div> „Dva týdny jsem se cítila špatně, byla jsem u lékaře, zjistili mi parazity. Podle receptu jsem zapíjela tabletky, ale s každým dnem mi bylo jen hůř. Nevím, co by se mnou bylo, kdybych se nedozvěděla o novém preparátu XX. Ten mě zachránil. Za měsíc jsem byla na testech a všechno bylo čisté. Teď budu pít XX jednou za rok - kvůli prevenci“.",
+
+"lt104": "<div class=\"slide-description-title\">Ondra, České Budějovice</div> „Náš kocour nesedí doma, rád běhá po dvoře. No a tak nám přinesl cizopasníky. Já jsem byl v pořádku, ale manželka trpěla. Nejdříve v noci skřípala zubami, potom začaly horečky a zvracení. Léčili jsme se společně, protože mohla velmi pravděpodobně nakazit i mě. Pili jsme XX a všechno nakonec zmizelo“.",
+
+"lt105": "<div class=\"slide-description-title\">Helena, Brno</div>  „Tento prostředek se mi opravdu zalíbil. Pili jsme ho s manželem oba. U každého z nás se to projevovalo jinak: objevoval se studený pot, byla řídká stolice, objevilo se i trochu akné a hrozná bolest hlavy. Už po 1,5 týdne zhoršení pominulo a teď se oba cítíme skvěle! XX funguje velmi delikátně, ale silně a sebevědomě“.",
+
+"lt106": "<div class=\"slide-description-title\">Petra, Ústí nad Orlicí</div> „Dva týdny jsem se cítila špatně, byla jsem u lékaře, zjistili mi parazity. Podle receptu jsem zapíjela tabletky, ale s každým dnem mi bylo jen hůř. Nevím, co by se mnou bylo, kdybych se nedozvěděla o novém preparátu XX. Ten mě zachránil. Za měsíc jsem byla na testech a všechno bylo čisté. Teď budu pít XX jednou za rok - kvůli prevenci“.",
+
+"lt107": "<div class=\"slide-description-title\">Ondra, České Budějovice</div>  „Náš kocour nesedí doma, rád běhá po dvoře. No a tak nám přinesl cizopasníky. Já jsem byl v pořádku, ale manželka trpěla. Nejdříve v noci skřípala zubami, potom začaly horečky a zvracení. Léčili jsme se společně, protože mohla velmi pravděpodobně nakazit i mě. Pili jsme XX a všechno nakonec zmizelo“.",
+
+"lt108": "<div class=\"slide-description-title\">Helena, Brno</div>  „Tento prostředek se mi opravdu zalíbil. Pili jsme ho s manželem oba. U každého z nás se to projevovalo jinak: objevoval se studený pot, byla řídká stolice, objevilo se i trochu akné a hrozná bolest hlavy. Už po 1,5 týdne zhoršení pominulo a teď se oba cítíme skvěle! XX funguje velmi delikátně, ale silně a sebevědomě“.",
+
+"lt110": "SILNÝ ÚDER PARAZITŮM",
+
+"lt111": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Normalizuje funkci trávícího traktu",
+
+"lt112": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Ničí všechny druhy cizopasníků",
+
+"lt113": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Odstraňuje produkty životní činnosti parazitů",
+
+"lt114": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Předchází opakované nákaze",
+
+"lt115": "SPECIÁLNÍ<br>NABÍDKA",
+
+"lt116": "do konce akce zbývá",
+
+"lt117": "<div class=\"price_old\"></div>  Běžná cena",
+
+"lt118": "<div class=\"price_main wow pulse\" data-wow-delay=\"2s\" data-wow-iteration=\"3\" style=\"visibility: hidden; animation-delay: 2s; animation-iteration-count: 3; animation-name: none;\"></div>  Cena v akci",
+
+"lt119": "Objednat",
+
+"lt120": "Jak pracujeme",
+
+"lt121": "Zanechte požadavek na našich stránkách",
+
+"lt122": "Operátor se s vámi spojí pro potvrzení objednávky",
+
+"lt123": "Platba při doručení objednávky"
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
